a58d1e5c6a06924846fb720fdf5fbf0ff2f7dc8c
rinarinar <75323458+rinarinar@users.noreply.github.com>
2025-09-11 11:33:00 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 // pages/_app.js
 import '../styles/global.css'
 import Head from 'next/head'
+import Navigation from '../components/Navigation'
 
 export default function App({ Component, pageProps }) {
   return (
@@ -10,7 +11,10 @@ export default function App({ Component, pageProps }) {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <title>陈文的个人网站</title>
+        <meta name="description" content="陈文的个人简历、每日一句和算命功能" />
       </Head>
+      <Navigation />
       <Component {...pageProps} />
     </>
   )
