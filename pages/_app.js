e62ca33bc3d236987cca27bf0c7506534a372ddf
rinarinar <75323458+rinarinar@users.noreply.github.com>
2025-09-11 11:22:03 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import '../styles/global.css';
+// pages/_app.js
+import '../styles/global.css'
+import Head from 'next/head'
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
